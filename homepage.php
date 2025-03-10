bd1d71d2a079adc07f3b10fec6bf6d5d6421e38c
karengeorgee <168434606+karengeorgee@users.noreply.github.com>
2025-03-10 19:40:13 +0200

    karen's homepage done

---
+++ homepage.php
@@ -0,0 +1,50 @@
+<?php include 'navbar.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homepage.css"> <!-- External CSS -->
+    <link rel="stylesheet" href="navbar.css">
+
+    <title>QuizzyVerse</title>
+</head>
+<body>
+    <div class="hero-section">
+        <div class="hero-content">
+            <h2>Introducing <span class="highlight">Instructional Suite</span></h2>
+            <h1>“Explore a universe of knowledge with QuizzyVerse.”</h1>
+            <p class="quote-author">- Almost everybody</p>
+            <p class="sub-text">Create and deliver bell-to-bell curriculum resources that meet the needs of every student.</p>
+            <div class="buttons">
+                <button class="teacher-btn">Sign up for free</button>
+                <button class="admin-btn">Learn more</button>
+            </div>
+        </div>
+    </div>
+    <section class="certificates">
+    <div class="certificates-container">
+        <div class="certificates-row">
+        <img src="trusted_apps_seal_2022with1edtech.png" alt="Certificate 1">
+        <img src="Screenshot 2025-03-10 182425.png" alt="Certificate 2">
+            <img src="CODIE_2022_finalist_black2-1200x731.jpg" alt="Certificate 3">
+            <img src="teens-selection-badge.png" alt="Certificate 4">
+            <img src="thumbnail_HIGHLY-COMMENDED-1.png" alt="Certificate 5">
+            <img src="Screenshot 2025-03-10 183048.png" alt="Certificate 6">
+            <img src="Screenshot 2025-03-10 183238.png" alt="Certificate 7">
+            <img src="Screenshot 2025-03-10 183258.png" alt="Certificate 8">
+        </div>
+        <p class="cert-text">
+            Trusted by teachers in <span class="purple-text">90% of U.S. Schools</span> and <span class="purple-text">150+ countries.</span>
+        </p>
+        <p class="certt-text">
+        Now we support every part of your lesson.
+        </p>
+        <p class="certtt-text">
+        Here’s how it works
+        </p>
+    </div>
+</section>
+
+</body>
+</html>
